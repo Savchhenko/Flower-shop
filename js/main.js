f2a140b2a4d4a86aa9a260ceb62bbf0bc2986ac7
Savchhenko <savchenko@phys.vsu.ru>
2021-02-06 00:18:30 +0300

    fancybox image gallery

--- js/main.js
+++ js/main.js
@@ -1,21 +1,60 @@
-const flowersSlider = new Swiper('.flowers-slider', {
-    // Slider's optional parameters
-    loop: true, //бесконечная прокрутка
-    slidesPerView: 6,
+$(document).ready(function () {
+    const flowersSlider = new Swiper('.flowers-slider', {
+        // Slider's optional parameters
+        loop: true, //бесконечная прокрутка
+        slidesPerView: 6,
+        
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
     
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+    const reviewsSlider = new Swiper('.reviews-slider', {
+        // Slider's optional parameters
+        loop: true, 
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-    // Slider's optional parameters
-    loop: true, 
-    slidesPerView: 1,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+    $("#review-1").on('click', function() {
+
+        $.fancybox.open([
+            {
+                // the first image
+                src  : '../img/flowers/bunch-1.jpg',
+                opts : {
+                    caption : 'First caption',
+                    thumb   : '../img/flowers/bunch-1.jpg'
+                }
+            },
+            {
+                // the second image
+                src  : '../img/flowers/bunch-2.jpg',
+                opts : {
+                    caption : 'Second caption',
+                    thumb   : '../img/flowers/bunch-2.jpg'
+                    }
+            },
+            {
+                // the third image
+                src  : '../img/flowers/bunch-3.jpg',
+                opts : {
+                    caption : 'Second caption',
+                    thumb   : '../img/flowers/bunch-3.jpg'
+                    }
+            }
+        ], {
+            loop : true,
+            thumbs : {
+                autoStart : true
+                }
+        });
+
+    });
+
 });
