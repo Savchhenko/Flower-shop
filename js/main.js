ec67ba6bad253b79c26b3a9d427f21187705fa98
Savchenko <savchenko@phys.vsu.ru>
2022-04-14 23:35:04 +0300

    changes eventListeners for counter buttons

--- js/main.js
+++ js/main.js
@@ -72,64 +72,20 @@ $(document).ready(function () {
     });
 
 });
+                
+const btnsCounterArray = [...document.querySelectorAll('.counter-button')];
 
-const btnCountMinus1 = document.getElementById('buttonCountMinus1'),
-    btnCountMinus2 = document.getElementById('buttonCountMinus2'),
-    btnCountMinus3 = document.getElementById('buttonCountMinus3'),
-    btnCountMinus4 = document.getElementById('buttonCountMinus4'),
-    btnCountMinus5 = document.getElementById('buttonCountMinus5'),
-    btnCountMinus6 = document.getElementById('buttonCountMinus6'),
-    btnCountMinus7 = document.getElementById('buttonCountMinus7'),
-    btnCountMinus8 = document.getElementById('buttonCountMinus8');
-const btnCountPlus1 = document.getElementById('buttonCountPlus1'),
-    btnCountPlus2 = document.getElementById('buttonCountPlus2'),
-    btnCountPlus3 = document.getElementById('buttonCountPlus3'),
-    btnCountPlus4 = document.getElementById('buttonCountPlus4'),
-    btnCountPlus5 = document.getElementById('buttonCountPlus5'),
-    btnCountPlus6 = document.getElementById('buttonCountPlus6'),
-    btnCountPlus7 = document.getElementById('buttonCountPlus7'),
-    btnCountPlus8 = document.getElementById('buttonCountPlus8');
-
-const minus = 'minus', 
-    plus = 'plus';
-    
-var strf = id => `${id}`;
-
-btnCountMinus1.addEventListener('click', function() {counter('buttonCountNumber1', strf, minus);});
-btnCountMinus2.addEventListener('click', function() {counter('buttonCountNumber2', strf, minus);});
-btnCountMinus3.addEventListener('click', function() {counter('buttonCountNumber3', strf, minus);});
-btnCountMinus4.addEventListener('click', function() {counter('buttonCountNumber4', strf, minus);});
-btnCountMinus5.addEventListener('click', function() {counter('buttonCountNumber5', strf, minus);});
-btnCountMinus6.addEventListener('click', function() {counter('buttonCountNumber6', strf, minus);});
-btnCountMinus7.addEventListener('click', function() {counter('buttonCountNumber7', strf, minus);});
-btnCountMinus8.addEventListener('click', function() {counter('buttonCountNumber8', strf, minus);});
-btnCountPlus1.addEventListener('click', function() {counter('buttonCountNumber1', strf, plus); });
-btnCountPlus2.addEventListener('click', function() {counter('buttonCountNumber2', strf, plus); });
-btnCountPlus3.addEventListener('click', function() {counter('buttonCountNumber3', strf, plus); });
-btnCountPlus4.addEventListener('click', function() {counter('buttonCountNumber4', strf, plus); });
-btnCountPlus5.addEventListener('click', function() {counter('buttonCountNumber5', strf, plus); });
-btnCountPlus6.addEventListener('click', function() {counter('buttonCountNumber6', strf, plus); });
-btnCountPlus7.addEventListener('click', function() {counter('buttonCountNumber7', strf, plus); });
-btnCountPlus8.addEventListener('click', function() {counter('buttonCountNumber8', strf, plus); });
-
-
-function counter(id, strf, e) {
-    console.log(strf(id)); // получаем id, который можно использовать в методе getElementById без ковычек
-
-    let counter = Number.parseInt(document.getElementById(strf(id)).innerText, 10);
-
-    if (e === 'plus') {
-        counter += 1;
-        document.getElementById(strf(id)).innerHTML = counter;
-    }
-    if(e === 'minus') {
-        if(counter != 0) {
-            counter -= 1;
-            document.getElementById(strf(id)).innerHTML = counter;
+btnsCounterArray.forEach((el) => {
+    el.addEventListener('click', (el) => {        
+        switch (el.target.textContent) {
+            case '+': 
+                el.target.previousElementSibling.innerHTML = Number(el.target.previousElementSibling.innerHTML) + 1;
+                break;
+            case '-': 
+                if (Number(el.target.nextElementSibling.innerHTML) > 0) {
+                    el.target.nextElementSibling.innerHTML = Number(el.target.nextElementSibling.innerHTML) - 1;
+                }
+                break;
         }
-    }
-}
-
-
-
-
+    });
+})
